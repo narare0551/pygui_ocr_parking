3eca6eec739b058bfbadae275a74524fdb07591c
USER <USER@DESKTOP-VVVPLSU>
2021-02-26 09:41:33 +0900

    template load

--- opencv_main.py
+++ opencv_main.py
@@ -54,7 +54,7 @@ def templates(a):
 
 
     # 저장경로에서 0-9까지의 template을 들고온다.
-    templatepath = ('C:\\Users\\USER\\Desktop\\test\\verniersegment\\' + str(a) + 'vernier.png')  # trainImage
+    templatepath = ('C:\\Users\\USER\\Desktop\\test\\template\\facnroll7seg_v20\\20FACNROLL_'+str(a)+'.png')  # trainImage
     # 이미지 템플렛을 로딩해서 참고한다.
     template = cv2.imread(templatepath, 1)  # trainImage
     # print('비교하는 숫자',a)
@@ -64,7 +64,7 @@ def templates(a):
     template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
     cv2.imshow('template', template)
     # 테두리를 감지한다  -> 인식률을 높여줌 (template이 이미 테두리 처리 된 거라서 일단 주석처리함)
-    template = cv2.Canny(template, 50, 200)
+    #emplate = cv2.Canny(template, 50, 200)
     print(template)
 
     # templates.append(template)
